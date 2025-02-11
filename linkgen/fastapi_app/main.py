5cd269915ef26a8aa3ece86da0ac40c83c95f2b3
Preston <prestonosoro56@gmail.com>
2025-02-11 20:54:24 +0300

    chore(main): update FastAPI app to include profile routes

--- linkgen/fastapi_app/main.py
+++ linkgen/fastapi_app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from campaigns.routes.campaign import campaign_router
 from campaigns.routes.message_template import message_template_router
+from profiles.routes.profile import profile_router
+
 
 app = FastAPI()
 
 app.include_router(campaign_router, prefix="/api/v1/campaigns", tags=["Campaigns"])
 app.include_router(message_template_router, prefix="/api/v1/message_templates", tags=["Message Templates"])
+app.include_router(profile_router, prefix="/api/v1/profiles", tags=["Profiles"])
